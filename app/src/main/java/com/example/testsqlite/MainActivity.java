54549a27a5360cf8d30d5f0a0cf1e9ffa031e227
ChainarongN <36422853+ChainarongN@users.noreply.github.com>
2020-07-21 20:28:59 +0700

    update

---
+++ app/src/main/java/com/example/testsqlite/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.testsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    DatabaseHelper mDatabaseHelper;
+    private Button btnAdd;
+    private EditText editText;
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        mListView = (ListView) findViewById(R.id.listView);
+        mDatabaseHelper = new DatabaseHelper(this);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newEntry = editText.getText().toString();
+                if (editText.length() != 0) {
+                    AddData(newEntry);
+                    editText.setText("");
+                } else {
+                    toastMessage("You must put something in the text field!");
+                }
+
+                mListView.setAdapter(null);
+                populateListView();
+            }
+        });
+
+        populateListView();
+    }
+
+    public void AddData(String newEntry) {
+        boolean insertData = mDatabaseHelper.addData(newEntry);
+
+        if (insertData) {
+            toastMessage("Data Successfully Inserted!");
+        } else {
+            toastMessage("Something went wrong");
+        }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void populateListView() {
+        Log.d(TAG, "populateListView: Displaying data in the ListView.");
+
+        //get the data and append to a list
+        Cursor data = mDatabaseHelper.getData();
+        ArrayList<String> listData = new ArrayList<>();
+        while(data.moveToNext()){
+            //get the value from the database in column 1
+            //then add it to the ArrayList
+            listData.add(data.getString(1));
+        }
+        //create the list adapter and set the adapter
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+        mListView.setAdapter(adapter);
+
+        //set an onItemClickListener to the ListView
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = adapterView.getItemAtPosition(i).toString();
+                Cursor data = mDatabaseHelper.getItemID(name);
+                int itemID = -1;
+                while(data.moveToNext()){
+                    itemID = data.getInt(0);
+                }
+
+                if(itemID > -1){
+                    mDatabaseHelper.deleteName(itemID,name);
+                    toastMessage("removed from database");
+
+                    mListView.setAdapter(null);
+                    populateListView();
+                }
+                else{
+                    toastMessage("No ID associated with that name");
+                }
+
+            }
+        });
+    }
+}
+
+//    #Update
+//                 String item = editable_item.getText().toString();
+//                    if(!item.equals("")){
+//                        mDatabaseHelper.updateName(item,itemID,name);
+//                        }else{
+//                        toastMessage("You must enter a name");
+//                        }
